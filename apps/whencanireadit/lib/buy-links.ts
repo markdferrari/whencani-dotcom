18cff2d9ab72b3057f8e99d5ce4b92dd028a5fe8
Mark Ferrari <markdferrari@gmail.com>
2026-02-16 20:42:00 +0000

    feat: include us placeholder id
    Signed-off-by: Mark Ferrari <markdferrari@gmail.com>

--- apps/whencanireadit/lib/buy-links.ts
+++ apps/whencanireadit/lib/buy-links.ts
@@ -1,23 +1,40 @@
 import type { Book, BuyLink } from './types';
 
-export function generateBuyLinks(book: Book): BuyLink[] {
+export function generateBuyLinks(book: Book, countryCode?: string): BuyLink[] {
   const links: BuyLink[] = [];
   const isbn = book.isbn13 ?? book.isbn10;
   const bookshopUkAffiliateTag = '17007';
-  if (isbn) {
-    links.push({
-      name: 'Support your local - buy from Bookshop.org',
-      url: `https://uk.bookshop.org/a/${bookshopUkAffiliateTag}/${isbn}`,
-      icon: 'bookshop',
-    });
+  const bookshopUsAffiliateTag = '17007TBC';
+
+  if (isbn && book.title && book.authors && book.authors.length > 0) {
+    if (countryCode === 'GB') {
+      links.push({
+        name: 'Support your local - buy from Bookshop.org',
+        url: `https://uk.bookshop.org/a/${bookshopUkAffiliateTag}/${isbn}`,
+        icon: 'bookshop',
+      });
+    } else {
+      links.push({
+        name: 'Support your local - buy from Bookshop.org',
+        url: `https://bookshop.org/a/${bookshopUsAffiliateTag}/${isbn}`,
+        icon: 'bookshop',
+      });
+    }
   }
 
-  // Always add Kindle Unlimited link
+  // Always add Kindle Unlimited 
+  if (countryCode === 'GB') {
+    links.push({
+      name: 'Kindle Unlimited',
+      url: 'https://www.amazon.co.uk/kindle-dbs/hz/signup?tag=whencaniplayg-21',
+      icon: 'kindle',
+    });
+  } else {
   links.push({
     name: 'Kindle Unlimited',
-    url: 'https://www.amazon.co.uk/kindle-dbs/hz/signup?tag=whencaniplayg-21',
+    url: 'https://www.amazon.com/kindle-dbs/hz/signup?tag=whencaniplayg-21',
     icon: 'kindle',
-  });
+  })};
 
   // Always add Audible link
   links.push({
